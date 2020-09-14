3ef73389792e920093eb6d7bacbc1bf580b827e2
Wytamma Wirth <wytamma.wirth@me.com>
2020-09-14 22:51:45 +1000

    :art: long running example

---
+++ example_scripts/sleep.R
@@ -0,0 +1,21 @@
+# Sleeping for 10! seconds
+#< Sleeping for 1 seconds
+Sys.sleep(1)
+#< Sleeping for 2 seconds
+Sys.sleep(2)
+#< Sleeping for 3 seconds
+Sys.sleep(3)
+#< Sleeping for 4 seconds
+Sys.sleep(4)
+#< Sleeping for 5 seconds
+Sys.sleep(5)
+#< Sleeping for 6 seconds
+Sys.sleep(6)
+#< Sleeping for 7 seconds
+Sys.sleep(7)
+#< Sleeping for 8 seconds
+Sys.sleep(8)
+#< Sleeping for 9 seconds
+Sys.sleep(9)
+#< Sleeping for 10 seconds
+Sys.sleep(10)
